bff64ccdc24841e1c05b32a5544663b48eb1eecf
Steve Drucker <sdrucker@figleaf.com>
2016-10-14 21:45:39 -0400

    Walk 4-3 and Walk 4-4

---
+++ walk/walk4-5/classic/src/view/admin/preexistingconditions/PreExistingConditionsController.js
@@ -0,0 +1,5 @@
+Ext.define('PatientChart.view.admin.preexistingconditions.PreExistingConditionsController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.admin-preexistingconditions-preexistingconditions'
+    
+});
---
+++ walk/walk4-5/classic/src/view/patientinfo/media/MediaModel.js
@@ -0,0 +1,8 @@
+Ext.define('PatientChart.view.patientinfo.media.MediaModel', {
+    extend: 'Ext.app.ViewModel',
+    alias: 'viewmodel.patientinfo-media-media',
+    data: {
+        name: 'PatientChart'
+    }
+
+});
