404fde544bea4e427c00ce1eaba0e27e510f58d9
Steve Drucker <sdrucker@figleaf.com>
2016-10-27 04:45:07 -0500

    Fix for responsive buttons

--- snippets/walk11-4b.js
+++ snippets/walk11-4b.js
@@ -1,43 +1,51 @@
 initComponent: function() {
 
-    var vp = this.up('viewport');
+     var vp = this.up('viewport');
 
-    if (vp.getWidth() > vp.getHeight()) {
+     if (vp.getWidth() > vp.getHeight()) {
 
-        Ext.apply(this, {
-            layout: {
-                type: 'vbox',
-                align: 'stretch'
-            },
-            width: 170,
-            bodyPadding: 5
-        });
-        this.defaults.flex = null;
-        this.defaults.margin = '0 0 10 0';
+         Ext.apply(this, {
+             layout: {
+                 type: 'vbox',
+                 align: 'stretch'
+             },
+             width: 170,
+             bodyPadding: 5
+         });
+         this.defaults.flex = null;
+         this.defaults.margin = '0 0 10 0';
 
-    } else {
+     } else {
 
-        Ext.apply(this, {
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
-            height: 50,
-            bodyPadding: 0,
-            collapseMode: 'mini'
-        });
+         Ext.apply(this, {
+             layout: {
+                 type: 'hbox',
+                 align: 'stretch'
+             },
+             height: 50,
+             bodyPadding: 0,
+             collapseMode: 'mini'
+         });
 
-        this.defaults.flex = 1;
-        this.defaults.margin = '0 5 0 0';
+         this.defaults.flex = 1;
+         this.defaults.margin = '0 5 0 0';
 
-        // remove icons
-        for (var i = 0; i < this.items.length; i++) {
-            this.items[i].iconCls = null;
-            if (i == this.items.length - 1) {
-                this.items[i].margin = '0 0 0 0';
-            }
-        }
-    }
+     }
 
-    this.callParent(arguments);
-}
+     this.callParent(arguments);
+ },
+
+ listeners: {
+
+     'afterrender': function(cmp) {
+
+         var vp = this.up('viewport');
+         if (vp.getWidth() < vp.getHeight()) {
+             var buttons = cmp.query('button');
+             for (var i = 0; i < buttons.length; i++) {
+                 buttons[i].setIconCls('');
+             }
+         }
+
+     }
+ }
